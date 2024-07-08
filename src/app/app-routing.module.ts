a62368929104a338971837609f057622a888c17a
German Aliaga <germanyp20l@gmail.com>
2024-07-08 10:54:18 +0200

    Creacion y navegacion del menu en Boostrap..  con los RouterLink.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetaliComponent } from './product-detali/product-detali.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'products/:productId', component: ProductDetaliComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', redirectTo:'home', pathMatch: 'full'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductDetaliComponent } from './product-detali/product-detali.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    ProductDetaliComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
