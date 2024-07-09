f1d2a76b0f0a9e1870fdcd245929883184b7cac5
German Aliaga <germanyp20l@gmail.com>
2024-07-09 13:08:17 +0200

    Estructuraa de control del arbol DOM  NG

--- src/app/products/products.mock.ts
+++ src/app/products/products.mock.ts
@@ -3,7 +3,7 @@ export const productList : Product[] =[
     {id: 2, name: 'teclado', price: 12, description: ' teclado de computadora Logitech '},
     {id: 3, name: 'mouse', price: 10, description: 'Raton o maus e Logitech'},
     {id: 4, name: 'webcam', price: 20, description: 'Camara web conexion USB par< computadora marca logitech'},
-    {id: 5, name: 'monitor', price: 100, description: 'Monitor AOC 21 Pulgadas'},
+    {id: 5, name: 'monitor', price: 100 },
     {id: 6, name: 'impresora ', price: 70, description: ' impresora HP 4200'},
     {id: 7, name: 'Memoria-USB', price: 25, description: 'memoria usb de 64GB'},
     {id: 8, name: 'cable HDMI', price: 10, description: ' Cable conexion al monitor HDMI'}
@@ -12,5 +12,5 @@ export  interface Product{
      id:number; 
      name: string;
      price: number;
-     description: string;
+     description?: string;
 }
