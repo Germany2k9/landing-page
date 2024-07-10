e594e5ce6451178772932d4edf667ca8da8da80a
German Aliaga <germanyp20l@gmail.com>
2024-07-10 14:19:12 +0200

    PatchValue seteamos o parchamos una parte del formulario de contacto con los requiered.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,18 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
 
-  public usuario: any= {
-    nombre:'',
-    email:''
+  formularioContacto: FormGroup
+  usuarioActivo: any ={
+    nombre:'Pedro',
+    apellido: 'Perez',
+    DNI: '12345678',
+  } 
+
+  constructor (private Form: FormBuilder) {
+
+    this.formularioContacto = this.Form.group({
+      nombre:['', [Validators.required, Validators.minLength(3)]], 
+      apellido:['', [Validators.required, Validators.minLength(3)]], 
+      DNI:['', [Validators.required, Validators.minLength(3)]], 
+        email: ['', [Validators.required, Validators.email]]
+      
+    })
   }
+  
+  ngOnInit(): void {
+    this.formularioContacto.patchValue({
+
+      nombre: this.usuarioActivo.nombre,
+      apellido: this.usuarioActivo.apellido,
+      DNI: this.usuarioActivo.DNI
+    })
+      //this.formularioContacto.get('nombre')?.setValue(this.usuarioActivo)
+      // si ya tuvieras informacion del usuario que no keres modificar podes setearlo de esta forma .
+      this.formularioContacto.get('nombre')?.disable()
+      this.formularioContacto.get('apellido')?.disable()
+      this.formularioContacto.get('DNI')?.disable()
+  }
+
+  hasErrors( controlName: string, errorType: string){
+    return this.formularioContacto.get(controlName)?.hasError(errorType) && this.formularioContacto.get(controlName)?.touched
+  }
+   
 
   enviar(){
-    console.log(this.usuario);
+    console.log(this.formularioContacto)
   }
+ 
+    
 }
