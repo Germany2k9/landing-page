cfaca99d2242363d0b8494c0ad91c377f770431e
German Aliaga <germanyp20l@gmail.com>
2024-07-11 12:25:37 +0200

    Formularios reactivos  donde el componente se mantiene escuchando una accion con el on change.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,35 +9,55 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ContactComponent implements OnInit{
 
   formularioContacto: FormGroup
-  usuarioActivo: any ={
-    nombre:'Pedro',
-    apellido: 'Perez',
-    DNI: '12345678',
-  } 
+  tipoDni:string ='DNI' 
+
+  // usuarioActivo: any ={
+  //   nombre:'Pedro',
+  //   apellido: 'Perez',
+  //   DNI: '12345678',
+  // } 
 
   constructor (private Form: FormBuilder) {
-
+    //podemos NO setear las validaciones 
     this.formularioContacto = this.Form.group({
-      nombre:['', [Validators.required, Validators.minLength(3)]], 
+      nombre:['', [Validators.required, Validators.minLength(3)]],
+      // xample  apellido: [''], 
       apellido:['', [Validators.required, Validators.minLength(3)]], 
-      DNI:['', [Validators.required, Validators.minLength(3)]], 
+      DNI:[''], 
+      tipoDni:[''], 
         email: ['', [Validators.required, Validators.email]]
       
     })
   }
   
   ngOnInit(): void {
-    this.formularioContacto.patchValue({
+    //Tambien podemos  setear validadores por fuera del constructor EJM
+    //this.formularioContacto.get('apellido')?.setValidators([Validators.required, Validators.minLength(3)]); 
+    //DEPENDIENDO EL TIPO DE PANTALLA QUE TENES UN CAMPO ES OBLIGATORIO O NO ES OBLIGATORIO
 
-      nombre: this.usuarioActivo.nombre,
-      apellido: this.usuarioActivo.apellido,
-      DNI: this.usuarioActivo.DNI
-    })
-      //this.formularioContacto.get('nombre')?.setValue(this.usuarioActivo)
-      // si ya tuvieras informacion del usuario que no keres modificar podes setearlo de esta forma .
-      this.formularioContacto.get('nombre')?.disable()
-      this.formularioContacto.get('apellido')?.disable()
-      this.formularioContacto.get('DNI')?.disable()
+    //if (this.usuarioActivo.apellido === '') this.formularioContacto.get('apellido')?.setValidators([Validators.required, Validators.minLength(3)]);
+    
+    // el caso contrario si nosotros queremos SACARLE la validacion
+    // this.formularioContacto.get('apellido')?.clearValidators()
+    // this.formularioContacto.get('apellido')?.updateValueAndValidity()
+
+
+    // this.formularioContacto.patchValue({
+
+    //   nombre: this.usuarioActivo.nombre,
+    //   //apellido: this.usuarioActivo.apellido,
+    //   DNI: this.usuarioActivo.DNI
+    // })
+    //   //this.formularioContacto.get('nombre')?.setValue(this.usuarioActivo)
+    //   // si ya tuvieras informacion del usuario que no keres modificar podes setearlo de esta forma .
+    //   this.formularioContacto.get('nombre')?.disable()
+    //   //this.formularioContacto.get('apellido')?.disable()
+    //   this.formularioContacto.get('DNI')?.disable()
+
+    //SUSCRIPCIONES
+    this.formularioContacto.get('tipoDni')?.valueChanges. subscribe(value =>{
+       this.tipoDni= value    
+      })
   }
 
   hasErrors( controlName: string, errorType: string){
